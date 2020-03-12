dfe633d1499910b73afd5c4a79bd680466f9a028
David Dumolo <ddumolo@hotmail.com>
2020-03-12 15:56:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function theBeatlesPlay(musicians, instruments) {
 
   return array;
 }
+
+function johnLennonFacts(facts) {
+  const array = []
+  let i = 0
+  while(i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return array;
+}
