e0d60f9999d897d6e123983903a6a565e0d855d8
David Dumolo <ddumolo@hotmail.com>
2020-03-12 15:42:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ function theBeatlesPlay(musicians, instruments) {
   let array = [];
   for(let i = 0; i < musicians.length; i++) {
     let string = "";
-    string += musicians[i];
     for(let j = 0; j < instruments.length; j++) {
-      string = string + " plays " + instruments[j];
+      string = musicians[i] + " plays " + instruments[j];
       array.push(string);
     }
   }
