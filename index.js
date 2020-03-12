2edbc5a9857a2b622bb6a7c81a1e2f6da3b0f4f7
David Dumolo <ddumolo@hotmail.com>
2020-03-12 15:40:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let array = [];
+  for(let i = 0; i < musicians.length; i++) {
+    let string = "";
+    string += musicians[i];
+    for(let j = 0; j < instruments.length; j++) {
+      string = string + " plays " + instruments[j];
+      array.push(string);
+    }
+  }
+  return array;
+}
